452bc2629b9d09f14eb6103e3bb333d5f738ed40
unknown <miintsrx@gmail.com>
2022-10-24 15:56:53 +0700

    add : register/login

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from "react";
 import Home from "./components/home";
 import Register from "./components/register";
+import Signin from "./components/signin";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import { AuthProvider } from "./contexts/AuthContext";
 import { ThemeProvider } from "react-bootstrap";
@@ -17,6 +18,7 @@ const App = () => {
           <Switch>
             <Route exact path="/" component={Home} />
             <Route exact path="/register" component={Register} />
+            <Route exact path="/signin" component={Signin} />
           </Switch>
         </Router>
       </AuthProvider>
--- src/components/signin.js
+++ src/components/signin.js
@@ -9,13 +9,7 @@ import { useAuth } from "../contexts/AuthContext";
 import Form from "react-bootstrap/Form";
 import Card from "react-bootstrap/Card";
 
-const Register = () => {
-  const { currentUser, testuser } = useAuth(); // use state,function from authcontext
-
-  useEffect(() => {
-    testuser(); // use function from authcontext
-  }, []);
-  console.log(currentUser); // use state from authcontext
+const Signin = () => {
 
   return (
     <div style={StyleRegister}>
@@ -27,16 +21,8 @@ const Register = () => {
                   <Form.Label>User Name</Form.Label>
                   <Form.Control type="username" />
                 </Form.Group>
-                <Form.Group className="mb-4 my-3" controlId="formGroupEmail">
-                  <Form.Label>Email Address</Form.Label>
-                  <Form.Control type="email" />
-                </Form.Group>
                 <Form.Group className="mb-4" controlId="formGroupPassword">
                   <Form.Label>Password</Form.Label>
-                  <Form.Control type="password"/>
-                </Form.Group>
-                <Form.Group className="mb-4" controlId="formGroupPassword">
-                  <Form.Label>Repeat Password</Form.Label>
                   <Form.Control type="password"/>
                 </Form.Group>
                 <Button variant="primary px-3" type="submit">Submit</Button>
@@ -48,7 +34,7 @@ const Register = () => {
   );
 };
 
-export default Register;
+export default Signin;
 const StyleRegister={
       backgroundImage: "url('https://www.stormforge.io/wp-content/uploads/2020/10/mountain-scape-uai-1440x960.jpg')",
       backgroundPosition: 'center',
